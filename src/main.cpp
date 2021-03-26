4eaad2cdbb68c9c5cd20b893d364c3a3370d4d1e
Paul Vizard <paulv@protolink.com>
2021-03-26 17:33:16 -0500

    Remove unneeded libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,46 @@
-#include <functional>
-#include <iostream>
+#include <imgui.h>
+#include <imgui-SFML.h>
 
-#include <spdlog/spdlog.h>
-#include <docopt/docopt.h>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
 
-static constexpr auto USAGE =
-  R"(Naval Fate.
-
-    Usage:
-          naval_fate ship new <name>...
-          naval_fate ship <name> move <x> <y> [--speed=<kn>]
-          naval_fate ship shoot <x> <y>
-          naval_fate mine (set|remove) <x> <y> [--moored | --drifting]
-          naval_fate (-h | --help)
-          naval_fate --version
- Options:
-          -h --help     Show this screen.
-          --version     Show version.
-          --speed=<kn>  Speed in knots [default: 10].
-          --moored      Moored (anchored) mine.
-          --drifting    Drifting mine.
-)";
-
-int main(int argc, const char **argv)
+int main()
 {
-  std::map<std::string, docopt::value> args = docopt::docopt(USAGE,
-    { std::next(argv), std::next(argv, argc) },
-    true,// show help if requested
-    "Naval Fate 2.0");// version string
+  sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3");
+  window.setFramerateLimit(60);
+  ImGui::SFML::Init(window);
 
-  for (auto const &arg : args) {
-    std::cout << arg.first << arg.second << std::endl;
+  sf::CircleShape shape(100.f);
+  shape.setFillColor(sf::Color::Green);
+
+  sf::Clock deltaClock;
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      ImGui::SFML::ProcessEvent(event);
+
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
+    }
+
+    ImGui::SFML::Update(window, deltaClock.restart());
+
+//    ImGui::ShowDemoWindow();
+
+    ImGui::Begin("Hello, world!");
+    ImGui::Button("Look at this pretty button");
+    ImGui::End();
+
+    window.clear();
+    window.draw(shape);
+    ImGui::SFML::Render(window);
+    window.display();
   }
 
+  ImGui::SFML::Shutdown();
 
-  //Use the default logger (stdout, multi-threaded, colored)
-  spdlog::info("Hello, {}!", "World");
-
-  fmt::print("Hello, from {}\n", "{fmt}");
+  return 0;
 }
