f9743484168b42f0c58bd57dc1e4d4a4f8528495
Paul Vizard <paul@golborne.com>
2021-03-26 20:47:20 -0500

    Fix build

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,39 @@
+#include <functional>
+#include <iostream>
+
+#include <spdlog/spdlog.h>
+#include <docopt/docopt.h>
+
 #include <imgui.h>
 #include <imgui-SFML.h>
-
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/System/Clock.hpp>
 #include <SFML/Window/Event.hpp>
 #include <SFML/Graphics/CircleShape.hpp>
 
-int main()
+int main()//int argc, const char **argv)
 {
-  sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3");
+  //std::map<std::string, docopt::value> args = docopt::docopt(USAGE,
+    //{ std::next(argv), std::next(argv, argc) },
+    //true,// show help if requested
+    //"Naval Fate 2.0");// version string
+
+  //for (auto const &arg : args) {
+    //std::cout << arg.first << arg.second << std::endl;
+  //}
+
+  //Use the default logger (stdout, multi-threaded, colored)
+  spdlog::info("Starting ImGui + SFML");
+
+  sf::RenderWindow window(sf::VideoMode(1024, 1024), "ImGui + SFML = <3");
   window.setFramerateLimit(60);
   ImGui::SFML::Init(window);
+  
+  constexpr auto scale_factor = 2;
+  ImGui::GetStyle().ScaleAllSizes(scale_factor);
+  ImGui::GetIO().FontGlobalScale = scale_factor;
 
-  sf::CircleShape shape(100.f);
+  sf::CircleShape shape(400.f);
   shape.setFillColor(sf::Color::Green);
 
   sf::Clock deltaClock;
@@ -27,8 +48,6 @@ int main()
     }
 
     ImGui::SFML::Update(window, deltaClock.restart());
-
-//    ImGui::ShowDemoWindow();
 
     ImGui::Begin("Hello, world!");
     ImGui::Button("Look at this pretty button");
